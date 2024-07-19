e98eb7f6cb3a123168d3d84e9ebca8b7d4fd1656
aijianiula0601@gmail.com <348348jia>
2024-07-19 09:18:27 +0800

    update

--- run_shells/sft_train_ultrachat_multigpu.sh
+++ run_shells/sft_train_ultrachat_multigpu.sh
@@ -14,21 +14,20 @@ cd ..
 
 output_dir="/mnt/cephfs/hjh/train_record/nlp/llamafactory/huggyllama_llama-7b/lora/sft"
 model_name_or_path="huggyllama/llama-7b"
-#model_name_or_path="/mnt/cephfs/hjh/huggingface_models/nlp/llama3/Meta-Llama-3-70B-Instruct"
+#model_name_or_path="/mnt/cephfs/hjh/huggingface_models/nlp/llama3/Meta-Llama-3-70B-Instruct" # 从挂载目录中加载模型，速度很慢
+#model_name_or_path="/data1/llm-ckpt/Higgs-Llama-3-70B"
 dataset_dir="./data"
 dataset_name="ultrachat_200k"
 
 
-DISTRIBUTED_ARGS="
-    --nproc_per_node 2 \
-    --nnodes 1 \
-    --node_rank 0 \
-    --master_addr localhost \
-    --master_port 26458
-    "
-
-CUDA_VISIBLE_DEVICES=6,7 \
-torchrun $DISTRIBUTED_ARGS src/train.py \
+#CUDA_VISIBLE_DEVICES=6,7 \
+torchrun \
+--nproc_per_node 8 \
+--nnodes 1 \
+--node_rank 0 \
+--master_addr localhost \
+--master_port 26458 \
+src/train.py \
   --stage sft \
   --do_train \
   --model_name_or_path ${model_name_or_path} \
