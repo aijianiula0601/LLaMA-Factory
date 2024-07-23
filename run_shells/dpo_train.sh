145dd1484547d98fc3f572b23e90183a3b6bf3b2
aijianiula0601@gmail.com <348348jia>
2024-07-23 19:49:02 +0800

    update

--- run_shells/dpo_train.sh
+++ run_shells/dpo_train.sh
@@ -10,46 +10,6 @@ cd ..
 
 output_dir="/mnt/cephfs/hjh/train_record/nlp/llamafactory/huggyllama_llama-7b/lora/dpo"
 model_name_or_path="/data1/llm-ckpt/Mistral-7B-v0.1"
-adapter_name_or_path="/mnt/cephfs/hjh/train_record/nlp/llamafactory/huggyllama_llama-7b/lora/sft"
-dataset_dir="./data"
-dataset_name="dpo_en_demo"
-
-#CUDA_VISIBLE_DEVICES=0 python src/train.py \
-#    --stage dpo \
-#    --do_train \
-#    --model_name_or_path ${model_name_or_path} \
-#    --adapter_name_or_path ${adapter_name_or_path} \
-#    --create_new_adapter \
-#    --dataset ${dataset_name} \
-#    --dataset_dir ${dataset_dir} \
-#    --template default \
-#    --finetuning_type lora \
-#    --lora_target q_proj,v_proj \
-#    --output_dir ${output_dir} \
-#    --overwrite_cache \
-#    --overwrite_output_dir \
-#    --cutoff_len 1024 \
-#    --preprocessing_num_workers 16 \
-#    --per_device_train_batch_size 1 \
-#    --per_device_eval_batch_size 1 \
-#    --gradient_accumulation_steps 8 \
-#    --lr_scheduler_type cosine \
-#    --logging_steps 10 \
-#    --warmup_steps 20 \
-#    --save_steps 100 \
-#    --eval_steps 100 \
-#    --evaluation_strategy steps \
-#    --load_best_model_at_end \
-#    --learning_rate 1e-5 \
-#    --num_train_epochs 1.0 \
-#    --max_samples 1000 \
-#    --val_size 0.1 \
-#    --plot_loss \
-#    --fp16
-
-
-output_dir="/mnt/cephfs/hjh/train_record/nlp/llamafactory/huggyllama_llama-7b/lora/dpo"
-model_name_or_path="/data1/llm-ckpt/Mistral-7B-v0.1"
 dataset_dir="./data"
 dataset_name="dpo_en_demo"
 
